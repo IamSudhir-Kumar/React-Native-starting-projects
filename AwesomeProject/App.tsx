43380b1fa96e61b64e6367e3296e8126ef25fceb
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-01-06 15:32:42 +0530

    first native prokect intaite

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello React native</Text>
+        <Text>Hello React native</Text>
+        <Text>Hello React native</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default App;
